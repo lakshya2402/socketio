216e08f60c11b4a47196d4c2a2ff5595f48b1c9d
lakshya2402 <lakshya2402@gmail.com>
2021-09-07 21:27:40 +0530

    mongo credintails removed

--- utils/properties.py
+++ utils/properties.py
@@ -4,5 +4,5 @@ DEFAULT_EXPIRE_TIME = 7 * 24 * 60 * 60 * 1000
 FILE_SUFFIXS = ("pdf", "jpeg", "png", "jpg")
 
 keysInformation ={"mongodb.auth.enabled": 'true',
-                  "mongodb.server.url": "mongodb+srv://mollify_socketio:<password>@cluster0.mvayx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"}
+                  "mongodb.server.url": ""}
 
